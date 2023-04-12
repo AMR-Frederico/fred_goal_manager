3ab138b8a0ef814c4c6026319f92c437b460dae4
DinossauroBebado <guipsilva32@gmail.com>
2023-04-12 23:58:39 +0000

    goal reset

--- scripts/goal_provider.py
+++ scripts/goal_provider.py
@@ -12,14 +12,19 @@ class GoalManager:
         # Subscribe to goals and goal reached topics
         rospy.Subscriber("/goal_manager/goals", PoseArray, self.goals_callback)
         rospy.Subscriber("/goal_manager/goal/reached", Bool, self.goal_reached_callback)
-
+        rospy.Subscriber("/goal_manager/goal/reset", Bool, self.reset_goals_callback)
         # Publisher for current goal topic
         self.current_goal_pub = rospy.Publisher("/goal_manager/goal/current", PoseStamped, queue_size=10)
-
+        self.mission_completed_pub = rospy.Publisher("/goal_manager/goal/mission_completed", Bool, queue_size=10)
         # Initialize class variables
         self.goals = []
         self.current_goal_index = 0
         self.goal_reached = False
+
+    def reset_goals_callback(self,msg):
+        reset = msg.data 
+        if(reset):
+             self.current_goal_index = 0
 
     def goals_callback(self, data):
         # Callback function to handle new goals received on "/goal_manager/goals" topic
@@ -39,9 +44,10 @@ class GoalManager:
                 self.current_goal_index += 1
                 self.publish_current_goal()
                 rospy.loginfo("Changed current goal to index %d", self.current_goal_index)
+                self.mission_completed_pub.publish(False)
             else:
                 rospy.loginfo("Reached the last goal.")
-
+                self.mission_completed_pub.publish(True)
                 # Reset the index to the first goal
                 # self.current_goal_index = 0
                 self.publish_current_goal()
@@ -59,6 +65,7 @@ class GoalManager:
 
         # Publish the message
         self.current_goal_pub.publish(current_goal)
+      
 
     def run(self):
         # Start ROS node and spin
